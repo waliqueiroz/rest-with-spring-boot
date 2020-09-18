1203949b88c2661c3f66dfb3a4caa6154c707637
Wali Queiroz <wali.santos@supermercadonow.com>
2020-09-18 20:02:17 -0300

    git ignore

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -1,20 +1,44 @@
 package br.com.erudio;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class MathController {
 
-	private static final String template = "Hello, %s";
-	private final AtomicLong counter = new AtomicLong();
+	@RequestMapping(value = "sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
 
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+
+		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+		return sum;
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null)
+			return 0D;
+
+		String number = strNumber.replaceAll(",", ".");
+
+		if (isNumeric(number))
+			return Double.parseDouble(number);
+
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null)
+			return false;
+
+		String number = strNumber.replaceAll(",", ".");
+
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 
 }
