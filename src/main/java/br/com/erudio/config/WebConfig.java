0ff26f65044d8caaec3bc24b5b7e782439dbaed1
Wali Queiroz <wali.santos@supermercadonow.com>
2020-09-24 09:32:16 -0300

    Content negotiaiton

--- src/main/java/br/com/erudio/config/WebConfig.java
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,18 +1,44 @@
 package br.com.erudio.config;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
+
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final MediaType MEDIA_TYPE_YAML = MediaType.valueOf("application/x-yaml");
+
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(false).ignoreAcceptHeader(false).defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+        // via extensão
+        // configurer.favorParameter(false).ignoreAcceptHeader(false).defaultContentType(MediaType.APPLICATION_JSON)
+        // .mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml",
+        // MediaType.APPLICATION_XML);
+
+        // Via QueryParam
+        // configurer.favorPathExtension(false).favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
+        // .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+        // .mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml",
+        // MediaType.APPLICATION_XML);
+
+        configurer.favorPathExtension(false).favorParameter(false).ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_YAML);
     }
 }
--- src/main/java/br/com/erudio/controller/PersonController.java
+++ src/main/java/br/com/erudio/controller/PersonController.java
@@ -22,24 +22,24 @@ public class PersonController {
 	@Autowired
 	private PersonService personService;
 
-	@GetMapping(produces = { "application/json", "application/xml" })
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
 	public List<PersonVO> findAll() {
 		return personService.findAll();
 	}
 
-	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml" })
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
 	public PersonVO findById(@PathVariable Long id) {
 		return personService.findById(id);
 	}
 
-	@PostMapping(consumes = { "application/json", "application/xml" }, produces = { "application/json",
-			"application/xml" })
+	@PostMapping(consumes = { "application/json", "application/xml", "application/x-yaml" }, produces = {
+			"application/json", "application/xml", "application/x-yaml" })
 	public PersonVO create(@RequestBody PersonVO person) {
 		return personService.create(person);
 	}
 
-	@PutMapping(consumes = { "application/json", "application/xml" }, produces = { "application/json",
-			"application/xml" })
+	@PutMapping(consumes = { "application/json", "application/xml", "application/x-yaml" }, produces = {
+			"application/json", "application/xml", "application/x-yaml" })
 	public PersonVO update(@RequestBody PersonVO person) {
 		return personService.create(person);
 	}
